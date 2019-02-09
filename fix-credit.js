58be6fd35c5ee666bd1e964213f314e3a72d00d6
Swizec Teller <swizec@swizec.com>
2019-02-09 14:58:55 -0800

    Make it actually work

--- fix-credit.js
+++ fix-credit.js
@@ -2,20 +2,20 @@ const CREDIT_ID = "tile-719933125";
 const CHECKING_ID = "tile-671419175";
 
 function value(node) {
-    return Number(
-        node
-            .text()
-            .replace("$", "")
-            .replace(",", "")
-    );
+    return Number(node.textContent.replace("$", "").replace(",", ""));
 }
 
 function getNode(account) {
     return document.querySelector(`#${account} .balance`);
 }
 
-const credit = getNode(CREDIT_ID),
-    checking = getNode(CHECKING_ID),
-    actualMoneys = value(checking) - value(credit);
+setTimeout(() => {
+    const credit = getNode(CREDIT_ID),
+        checking = getNode(CHECKING_ID);
 
-checking.text(`$${actualMoneys.toLocaleString("en")}`);
+    if (credit && checking) {
+        actualMoneys = value(checking) - value(credit);
+
+        checking.textContent = `$${actualMoneys.toLocaleString("en")}`;
+    }
+}, 3000);
